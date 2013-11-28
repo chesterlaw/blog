608db8019cc444b6acecec0ffd6d3150fe58ee58
Chester Law <chesterlaw@me.com>
2013-11-28 10:52:16 -0800

    extract layout from pages, improve .more + .back

--- app/controllers/w_controller.rb
+++ app/controllers/w_controller.rb
@@ -1,9 +1,13 @@
 class WController < ApplicationController
   def index
+    @chinese_title = ['羅', '彦', '章', '之', '網', '站', '。']
+
     render :layout => 'w'
   end
 
   def afighters
+    @chinese_title = ['甲', '級', '爭', '取', '者', '。']
+
     render :layout => 'w'
   end
 end
