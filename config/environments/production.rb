62e9fad6013c6e18be0ca179a1b190d5b005eea8
Chester Law <chesterlaw@me.com>
2013-12-03 20:51:04 -0800

    Add production code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Blog::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile << %w(afighters.js afighters.css w.js w.css)
 
   # Generate digests for assets URLs.
   config.assets.digest = true
