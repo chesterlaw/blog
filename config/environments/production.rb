d417b3f3177422320f33afdefef9ce3f51f43bde
Chester Law <chesterlaw@me.com>
2013-12-05 03:07:28 -0800

    edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Blog::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile << %w(afighters.js afighters.css w.js w.css)
 
   # Generate digests for assets URLs.
