93ffb72c5b4dd7c4923d5521bbeac93a344d78ad
Chester Law <chesterlaw@me.com>
2014-06-03 03:40:29 -0700

    route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 MyBlog::Application.routes.draw do
   root 'index#no_w'
 
-  get "w", to: "index#w"
-  get "w/2", to: "index#w2"
+  get "w", to: "index#w", as: "w"
+  get "w/2", to: "index#w2", as: "w2"
   get "w/a-fighters", to: "index#afighters", as: "afighters"
 
 end
