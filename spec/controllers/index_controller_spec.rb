58988357aa7eada574551f8a6cf299a7f68a4cbe
Chester Law <chesterlaw@me.com>
2016-01-31 13:29:00 -0800

    Update tests

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -22,31 +22,6 @@ describe IndexController, :type => :controller do
     expect_http_200
   end
 
-  it 'gets to index_3 successfully' do
-    get :index_3, format: :html
-    expect_http_200
-  end
-
-  it 'gets to index_4 successfully' do
-    get :index_4, format: :html
-    expect_http_200
-  end
-
-  it 'gets to index_5 successfully' do
-    get :index_5, format: :html
-    expect_http_200
-  end
-
-  it 'gets to index_6 successfully' do
-    get :index_6, format: :html
-    expect_http_200
-  end
-
-  it 'gets to index_7 successfully' do
-    get :index_7, format: :html
-    expect_http_200
-  end
-
   it 'gets to afighters successfully' do
     get :afighters, format: :html
     expect_http_200
