fb603e51dd33330744d6a57b166edda0038376d5
Chester Law <chesterlaw@me.com>
2016-01-31 19:25:39 -0800

    Remove old routes from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,6 @@ MyBlog::Application.routes.draw do
     get '', to: "index#index", as: :index
     get 'new', to: "index#index_new", as: :index_new
     get "2", to: "index#index_2", as: "index_2"
-    get "3", to: "index#index_3", as: "index_3"
-    get "4", to: "index#index_4", as: "index_4"
-    get "5", to: "index#index_5", as: "index_5"
-    get "6", to: "index#index_6", as: "index_6"
-    get "7", to: "index#index_7", as: "index_7"
     get "a-fighters", to: "index#afighters", as: "afighters"
   end
 
