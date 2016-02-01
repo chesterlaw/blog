83ef63949ab00c070f2024eb0321165187bfbe07
Chester Law <chesterlaw@me.com>
2016-01-31 20:30:39 -0800

    Improve get_chinese_title method

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -15,27 +15,26 @@ class IndexController < ApplicationController
   private
     def get_chinese_title
       month = Time.now.strftime("%m")
-      chinese_months = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十,一", "十,二"]
+      chinese_months = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二"]
       month_index = month.to_i - 1
       chinese_month = chinese_months[month_index]
 
       day = Time.now.strftime("%d")
       chinese_days = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十",
-                      "十,一", "十,二", "十,三", "十,四", "十,五", "十,六", "十,七", "十,八", "十,九", "二,十",
-                      "二,十,一", "二,十,二", "二,十,三", "二,十,四", "二,十,五", "二,十,六", "二,十,七", "二,十,八", "二,十,九", "三,十",
-                      "三,十,一"]
+                      "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "二十",
+                      "二十一", "二十二", "二十三", "二十四", "二十五", "二十六", "二十七", "二十八", "二十九", "三十", "三十一"]
       day_index = day.to_i - 1
       chinese_day = chinese_days[day_index]
 
-      @date = "今,天,是,#{chinese_month},月,#{chinese_day},日,。"
-      @message_1 = "「,甲,級,爭,取,者,」,是,我,2,0,0,9,年,創,作,的,一,部,漫,畫,。"
-      @message_2 = "藝/術/復/活/,/現/光/榮/回/歸/,/原/作/版/本/回/到/互/聯/網/！"
+      @date = "今天是#{chinese_month}月#{chinese_day}日。"
+      @message_1 = "「甲級爭取者」是我2009年創作的一部漫畫。"
+      @message_2 = "藝術復活,現光榮回歸,原作版本回到互聯網！"
 
-      @date = @date.split(",")
-      @h1_1 = '這/是/羅/彦/章/之/網/站/,'.split("/")
+      @date = @date.split('')
+      @h1_1 = '這是羅彦章之網站,'.split('')
       @h1_2 = '真,的,歡,迎,你,來,到,參,觀,!!'.split(",")
-      @message_1 = @message_1.split(",")
-      @message_2 = @message_2.split("/")
+      @message_1 = @message_1.split('')
+      @message_2 = @message_2.split('')
     end
 
     def get_mewtwo # mewtwo
