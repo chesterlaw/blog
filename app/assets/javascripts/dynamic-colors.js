3b5856da68b98f14f5b6be061c75d33eeadc99e1
Chester Law <chesterlaw@me.com>
2016-03-01 18:59:29 +0900

    Make dynamic-colors ITCSS compliant

--- app/assets/javascripts/dynamic-colors.js
+++ app/assets/javascripts/dynamic-colors.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $('.color-choice').on('click', function() {
+  $('.color-chooser__color-choice').on('click', function() {
     var color = $(this).css('background-color');
-    $(this).parents('.category').children('h1').css('color', color);
+    $(this).siblings('.chosen-color').css('color', color);
   });
 });
