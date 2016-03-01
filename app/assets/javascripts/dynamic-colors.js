af3df396edf5104cb1da4f399c1b4f6a300c4b09
Chester Law <chesterlaw@me.com>
2016-03-01 19:11:29 +0900

    Only run color-chooser on dynamic-colors route

--- app/assets/javascripts/dynamic-colors.js
+++ app/assets/javascripts/dynamic-colors.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  $('.color-chooser__color-choice').on('click', function() {
-    var color = $(this).css('background-color');
-    $(this).siblings('.color-chooser__chosen-color').css('color', color);
-  });
+  // Run main function below only on dynamic-colors page.
+  if ($('body').hasClass('dynamic-colors')) { main(); }
+
+  function main() {
+    $('.color-chooser__color-choice').on('click', function() {
+      var color = $(this).css('background-color');
+      $(this).siblings('.color-chooser__chosen-color').css('color', color);
+    });
+  };
 });
